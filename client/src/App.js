fa8336d486f4d5ad6012625ec462ad0a78fd8ade
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-07-03 12:37:51 -0400

    Added rtmpserver

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import './App.css';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import StreamCreate from './components/streams/StreamCreate';
 import StreamEdit from './components/streams/StreamEdit';
 import StreamList from './components/streams/StreamList';
@@ -9,17 +9,20 @@ import StreamDelete from './components/streams/StreamDelete';
 import StreamShow from './components/streams/StreamShow';
 import Header from './components/Header';
 import history from './history';
+
 const App = () => {
     return (
         <div>
             <Router history = {history}>
                 <div>
                     <Header />
+                    <Switch>
                     <Route path='/' exact component={StreamList} />
                     <Route path='/streams/new' exact component={StreamCreate} />
                     <Route path='/streams/edit/:id' exact component={StreamEdit} />
                     <Route path='/streams/delete/:id' exact component={StreamDelete} />
                     <Route path='/streams/:id' exact component={StreamShow} />
+                    </Switch>
                 </div>
             </Router>
         </div>
